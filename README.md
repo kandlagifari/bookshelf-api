a33e0f62903bb14e74f5d0f73f7d2895ef5df0b8
Kandla Gifari Akbar <kandlagifari@gmail.com>
2023-11-14 05:35:18 +0700

    migrate git

---
+++ README.md
@@ -0,0 +1,3 @@
+# Bookself API
+
+Project Submission for [Belajar Membuat Aplikasi Back-End untuk Pemula](https://www.dicoding.com/academies/261).
---
+++ src/server.js
@@ -0,0 +1,22 @@
+const Hapi = require('@hapi/hapi');
+const routes = require('./routes');
+
+const init = async () => {
+  const server = Hapi.server({
+    port: 9000,
+    host: process.env.DEPLOY === 'EC2' ? '0.0.0.0' : 'localhost',
+    routes: {
+      cors: {
+        origin: ['*'],
+      },
+    },
+  });
+
+  server.route(routes);
+
+  await server.start();
+  // eslint-disable-next-line no-console
+  console.log(`Server berjalan pada ${server.info.uri}`);
+};
+
+init();
