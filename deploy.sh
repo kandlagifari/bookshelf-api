63c6f0079d65fcba71c80a35ae9ba25f94cc58bc
Kandla Gifari Akbar <kandlagifari@gmail.com>
2023-11-14 07:02:51 +0700

    deploy lambda

---
+++ deploy.sh
@@ -0,0 +1,21 @@
+function deploy {
+  # Generate a version number based on a date timestamp so that it's unique
+  TIMESTAMP=$(date +%Y%m%d%H%M%S)
+  # Run the npm commands to install dependencies
+  npm i && \
+  # Create a dist folder and copy only the js files to dist.
+  # AWS Lambda does not have a use for a package.json or typescript files on runtime.
+  mkdir dist && \
+  cp -r ./src/*.js dist/ && \
+  cp -r ./node_modules dist/ && \
+  cd dist/ &&\
+  find . -name "*.zip" -type f -delete && \
+  # Zip everything in the dist folder and
+  zip -r ../terraform/zips/lambda_function_"$TIMESTAMP".zip . && \
+  cd ../ && rm -rf dist && \
+  cd terraform/ && \
+  terraform plan -input=false -var lambdasVersion="$TIMESTAMP" -out=./tfplan && \
+  terraform apply -input=false ./tfplan
+}
+
+deploy
--- terraform/main.tf
+++ terraform/main.tf
@@ -24,3 +24,58 @@ resource "aws_s3_bucket" "terraform_state" {
     prevent_destroy = true
   }
 }
+
+resource "aws_iam_role" "hapi_lambda_role" {
+  name               = "hapi-lambda-role"
+  assume_role_policy = jsonencode({
+    Version   = "2012-10-17"
+    Statement = [
+      {
+        Action    = "sts:AssumeRole"
+        Effect    = "Allow"
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_lambda_function" "hapi_lambda" {
+  filename      = "zips/lambda_function_${var.lambdasVersion}.zip"
+  function_name = "hapi-lambda-function"
+  role          = aws_iam_role.hapi_lambda_role.arn
+  handler       = "index.handler"
+  runtime       = "nodejs18.x"
+  memory_size   = 1024
+  timeout       = 300
+}
+
+resource "aws_lambda_function_url" "hapi_lambda_funtion_url" {
+  function_name      = aws_lambda_function.hapi_lambda.id
+  authorization_type = "NONE"
+}
+
+resource "aws_cloudwatch_log_group" "hapi_lambda_loggroup" {
+  name              = "/aws/lambda/${aws_lambda_function.hapi_lambda.function_name}"
+  retention_in_days = 1
+}
+
+data "aws_iam_policy_document" "hapi_lambda_policy" {
+  statement {
+    actions = [
+      "logs:CreateLogStream",
+      "logs:PutLogEvents"
+    ]
+    resources = [
+      aws_cloudwatch_log_group.hapi_lambda_loggroup.arn,
+      "${aws_cloudwatch_log_group.hapi_lambda_loggroup.arn}:*"
+    ]
+  }
+}
+
+resource "aws_iam_role_policy" "hapi_lambda_role_policy" {
+  policy = data.aws_iam_policy_document.hapi_lambda_policy.json
+  role   = aws_iam_role.hapi_lambda_role.id
+  name   = "hapi-lambda-policy"
+}
